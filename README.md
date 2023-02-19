b082c0a8add4e62a4068dfaa68ca10bfc481b5e6
Siddhant <siddhant@fastmail.com>
2023-02-19 12:25:22 +0100

    feat: analytics endpoint

---
+++ README.md
@@ -0,0 +1,3 @@
+# ingest-worker
+
+A Worker to send analytics/logs/traces/metrics from all my projects to Telegraf/InfluxDB
---
+++ src/index.ts
@@ -0,0 +1,127 @@
+import { z } from "zod";
+
+export interface Env {
+  // Secrets
+  ACCESS_CLIENT_ID: string;
+  ACCESS_CLIENT_SECRET: string;
+
+  // Vars
+  ALLOWED_ORIGINS: string[];
+  TELEGRAF_URL: string;
+}
+
+const analyticsSchema = z.discriminatedUnion("type", [
+  z.object({
+    type: z.literal("page_view"),
+    path: z.string(),
+  }),
+]);
+
+const logsSchema = z.object({
+  service: z.enum(["raft"]),
+});
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    if (request.method !== "POST") {
+      return new Response("Bad method", { status: 400 });
+    }
+
+    const origin = request.headers.get("Origin") ?? "";
+    if (!env.ALLOWED_ORIGINS.includes(origin)) {
+      return new Response("Bad origin", { status: 400 });
+    }
+
+    const url = new URL(request.url);
+    const params = await request.json();
+
+    if (url.pathname === "/a") {
+      const result = analyticsSchema.safeParse(params);
+      if (!result.success) {
+        return new Response("Bad data", { status: 400 });
+      }
+
+      const ip = request.headers.get("cf-connecting-ip") ?? "";
+      const userAgent = request.headers.get("user-agent") ?? "";
+      const country = request.headers.get("cf-ipcountry") ?? "unknown";
+
+      const work = analytics(env, origin, ip, userAgent, country, result.data);
+      ctx.waitUntil(work);
+    } else if (url.pathname === "/l") {
+      const result = logsSchema.safeParse(params);
+      if (!result.success) {
+        return new Response("Bad data", { status: 400 });
+      }
+
+      const work = logs(env, result.data);
+      ctx.waitUntil(work);
+
+      return new Response("Unimplemented", { status: 501 });
+    } else {
+      return new Response("Bad route", { status: 400 });
+    }
+
+    return new Response(null, {
+      status: 202,
+      headers: {
+        "access-control-allow-origin": origin,
+        "access-control-allow-methods": "POST",
+      },
+    });
+  },
+};
+
+async function analytics(
+  env: Env,
+  origin: string,
+  ip: string,
+  userAgent: string,
+  country: string,
+  data: z.infer<typeof analyticsSchema>
+): Promise<void> {
+  const encoder = new TextEncoder();
+  const today = new Date();
+
+  /**
+   * For every origin that reports analytics, visitors get a unique ID every day.
+   * We don't log their IPs / UserAgents, but we do use them to calculate their IDs.
+   * Visitor IDs let us determine uniqueness.
+   *
+   * This is exactly the strategy Plausible uses, and is a great balance between
+   * usefulness and privacy.
+   */
+  const visitorDigest = await crypto.subtle.digest(
+    "SHA-256",
+    encoder.encode(today.toDateString() + origin + ip + userAgent)
+  );
+  const visitorId = Array.from(new Uint8Array(visitorDigest))
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+
+  await telegraf(
+    env,
+    `${data.type},bucket=analytics,site=${origin},visitor=${visitorId} path="${
+      data.path
+    }",location="${country}" ${today.getTime()}`
+  );
+}
+
+async function logs(
+  env: Env,
+  data: z.infer<typeof logsSchema>
+): Promise<void> {}
+
+function telegraf(env: Env, body: string) {
+  return fetch(env.TELEGRAF_URL, {
+    method: "POST",
+    headers: {
+      "cf-access-client-id": env.ACCESS_CLIENT_ID,
+      "cf-access-client-secret": env.ACCESS_CLIENT_SECRET,
+    },
+    body,
+  });
+}

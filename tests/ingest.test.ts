3e97ae8af4873a8bd9e777f331b44d22cc4b94d5
Siddhant <git@sdnts.dev>
2023-09-14 11:02:00 -0500

    improve: tests for metrics

--- tests/ingest.test.ts
+++ tests/ingest.test.ts
@@ -210,3 +210,50 @@ test.describe("analytics", async () => {
     );
   });
 });
+
+test.describe("metrics", async () => {
+  test("no body", async ({ worker }) => {
+    const res = await worker.fetch("/m", {
+      method: "POST",
+    });
+
+    expect(res.status).toBe(400);
+    await expect(res.text()).resolves.toBe("Bad data");
+  });
+
+  test("incorrect schema", async ({ worker }) => {
+    const res = await worker.fetch("/m", {
+      method: "POST",
+      body: JSON.stringify({}),
+    });
+
+    expect(res.status).toBe(400);
+    await expect(res.text()).resolves.toBe("Bad data");
+  });
+
+  test("request", async ({ worker, request }) => {
+    const res = await worker.fetch("/m", {
+      method: "POST",
+      body: JSON.stringify({
+        name: "request",
+        origin: "https://blob.city",
+        method: "PUT",
+        path: "/tunnel",
+        status: 101,
+        fields: { rayId: "abcd", tunnelId: "1234", peerId: "1" },
+      }),
+    });
+    expect(res.status).toBe(202);
+
+    await worker.waitUntilExit();
+
+    const upstreamRequest = await request
+      .get("http://localhost:8888/get")
+      .then((r) => r.json());
+    expect(upstreamRequest).not.toBeUndefined();
+    expect(upstreamRequest.method).toBe("POST");
+    expect(upstreamRequest.body).toMatch(
+      `request,bucket=metrics,origin=https://blob.city,method=PUT,path=/tunnel,status=101 rayId="abcd",tunnelId="1234",peerId="1"`,
+    );
+  });
+});
